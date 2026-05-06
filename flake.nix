3c7ae5af91bcdcec4ce7542a3b5cb761b2a72938
adam-coates <123807847+adam-coates@users.noreply.github.com>
2026-05-07 00:02:35 +0200

    remove neovim integration as its part of the main neovim config

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Typst development environment with tinymist LSP and Neovim integration";
+  description = "Typst development environment with tinymist LSP";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -21,13 +21,6 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-
-          # Small Neovim plugin: typst helpers + tinymist LSP setup
-          typst-nvim-plugin = pkgs.vimUtils.buildVimPlugin {
-            pname = "typst-nvim";
-            version = "0.1.0";
-            src = ./plugin;
-          };
         in
         {
           default = pkgs.mkShell {
@@ -36,32 +29,6 @@
               pkgs.tinymist
               pkgs.zathura
             ];
-
-            shellHook = ''
-              echo "typst $(typst --version)"
-              echo "tinymist available: $(which tinymist)"
-              echo ""
-              echo "Neovim plugin at: ${typst-nvim-plugin}"
-              echo "Add it to your neovim runtimepath or copy plugin/typst.lua"
-              echo "into your own config."
-            '';
-          };
-        }
-      );
-
-      # Expose the plugin as a package so you can reference it from your
-      # NixOS/home-manager neovim config, e.g.:
-      #   programs.neovim.plugins = [ typst-flake.packages.${system}.typst-nvim-plugin ];
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          typst-nvim-plugin = pkgs.vimUtils.buildVimPlugin {
-            pname = "typst-nvim";
-            version = "0.1.0";
-            src = ./plugin;
           };
         }
       );
